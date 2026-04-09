7ca739126245be6af9b85133a1e7ca676ba1eb5d
Nguyen Tien Dat <dat243171@gmail.com>
2026-04-09 22:50:03 +0700

    uploaded 2 file ...

---
+++ Admin.java
@@ -0,0 +1,7 @@
+public class Admin extends Person {
+
+    
+}
+// File mình đang làm việc   ----- Staging Area ------ Commited     
+//                                  Admin.java
+//       Person.java
